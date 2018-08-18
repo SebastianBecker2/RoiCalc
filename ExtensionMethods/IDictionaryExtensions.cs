ded03ff999aba37b66e49e92f8a95b1ab08e4f47
Sebastian Becker <mail@sbecker.de.com>
2018-08-18 18:55:51 +0200

    Add GetValueOrDefault extension method for IDictionary
    Returns the value of the key provided or return Default(TValue).

---
+++ ExtensionMethods/IDictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class IDictionaryExtensions
+{
+    public static TValue GetValueOrDefault<TKey, TValue> 
+        (this IDictionary<TKey, TValue> dictionary,
+        TKey key,
+        TValue defaultValue)
+    {
+        return dictionary.TryGetValue(key, out TValue value) ? value : defaultValue;
+    }
+}
